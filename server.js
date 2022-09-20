223808da27b0f7d68397b0ad9c55d6dedb77111d
Jesse Meirink <jesse@meirink.com>
2022-09-20 13:49:28 -0500

    stub up express router and api routes

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
 
 // middleware
+app.use(express.json())
 app.use((req, res, next) => {
   console.log(req.method, req.path)
   next()
 })
 
 // routes
-app.get('/', (req, res) => {
-  res.json({mssg: 'welcome to the app'})
-})
+app.use('/api/workouts', workoutRoutes)
 
 // listen for requests
 app.listen(process.env.PORT, () => {
