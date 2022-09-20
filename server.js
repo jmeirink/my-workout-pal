f7b5d909da30d7e211f2e07a5c69bd2e26aaa22b
Jesse Meirink <jesse@meirink.com>
2022-09-20 14:31:02 -0500

    setup mongodb connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // express app
@@ -16,8 +17,17 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port 4000')
-})
+// connect to database
+mongoose.connect(process.env.DATABASE_URL)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to database & listening on port 4000')
+    })
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+
 
