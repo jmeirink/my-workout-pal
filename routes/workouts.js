d07716a71533f459b07f350c7366518dbdbf4720
Jesse Meirink <jesse@meirink.com>
2022-09-21 16:24:39 -0500

    stub up and code getWorkouts, getWorkout, and createWorkout controller functions

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,28 +1,15 @@
 const express = require('express')
-const Workout = require('../models/workoutModel')
+const { getWorkouts, getWorkout, createWorkout } = require('../controllers/workoutController')
 const router = express.Router()
 
 // GET all workouts
-router.get('/', (req, res) => {
-  res.json({mssg: 'GET all workouts'})
-})
+router.get('/', getWorkouts)
 
 // GET a single workout
-router.get('/:id', (req, res) => {
-  res.json({mssg: 'GET a single workout'})
-})
+router.get('/:id', getWorkout)
 
 // POST a new workout
-router.post('/', async (req, res) => {
-  const {title, reps, load} = req.body
-
-  try {
-    const workout = await Workout.create({title, reps, load})
-    res.status(200).json(workout)
-  } catch (err) {
-    res.status(400).json({error: err.message})
-  }
-})
+router.post('/', createWorkout)
 
 // DELETE a workout
 router.delete('/:id', (req, res) => {
